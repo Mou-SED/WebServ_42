039035363355b39d4323b24f28215f7a6bcbdbd8
Mou-SED <moseddik@student.1337.ma>
2023-06-09 17:43:36 +0100

    feat : Begin to create new core

--- include/Core.hpp
+++ include/Core.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Core.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 14:30:16 by moseddik          #+#    #+#             */
-/*   Updated: 2023/06/09 11:51:52 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2023/06/09 17:30:57 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,36 +21,17 @@ class Response;
 class Core
 {
 	private:
-		std::map<int, std::vector<Server> > _core;
-		std::map<int, std::vector<Server> > _socketFds;
-		std::set<int> _ports;
-		std::vector<struct pollfd> _pollfds;
+		std::map< int, std::vector<Server *> > _serversByFd;
+		std::set< int > _listenFds;
+		std::vector< struct pollfd > _pollFds;
 
 	public:
-		std::map<int, int> _FdsMap;
-		std::set<int> _pollFdsSet;
-		std::map<int, Request> requests;
-		std::map<int, Response> responses;
-		Core( std::vector<Server> servers );
-		~Core( void );
+		Core( std::vector< Server > const & servers );
 
-		void addServer( Server server );
-		void addSocketFd( void );
-
-		std::map<int, std::vector<Server> > const & get_core( void ) const;
-		std::map<int, Response> const & get_responses( void ) const;
-		void set_ports( int port );
-		bool find_port( int port );
-		void set_pollfds( void );
-		std::vector<struct pollfd> const & get_pollfds( void ) const;
-		// int get_socketFd( void ) const;
-		std::set<int> const & get_pollFdsSet( void ) const;
-		// std::map<int, Request> getRequests( void ) const;
-		Server	& getServer(int fd, const std::string & host );
-		void	closeConnection(int fd, Request &req);
-		void	cleanResponse(int fd);
-		void	removeRequest(int fd, std::map<int, Request> &Requests);
-		void	acceptConnection( const struct pollfd &pfd, int serverFd );
-		bool	readRequest(int fd);
-		void	sendResponse(Request &req, int fd);
+		std::pair<uint32_t, uint16_t> getHostAndPort( std::string const & host, std::string const & port );
+		void	addPollFd( int fd );
+		void	removePollFd( int fd );
+		void	start( void );
+		void	acceptNewConnection( int serverFd );
+		void	bindServerSockets( void );
 };
--- src/Core.cpp
+++ src/Core.cpp
@@ -3,325 +3,182 @@
 /*                                                        :::      ::::::::   */
 /*   Core.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 15:20:59 by moseddik          #+#    #+#             */
-/*   Updated: 2023/06/09 14:52:36 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2023/06/09 17:41:03 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Core.hpp"
 
-Core::Core(std::vector<Server> servers)
+std::pair<uint32_t, uint16_t> Core::getHostAndPort( std::string const & host, std::string const & port )
 {
-	for (size_t i = 0; i < servers.size(); i++)
-		this->addServer(servers[i]);
+	const struct addrinfo hint = (struct addrinfo){
+		.ai_family = AF_INET,
+		.ai_socktype = SOCK_STREAM,
+		.ai_protocol = 0
+	};
 
-	this->addSocketFd();
-	return;
+	struct addrinfo *res;
+	int ret = getaddrinfo( host.c_str(), port.c_str(), &hint, &res );
+	if ( ret != 0 )
+		throw std::runtime_error( gai_strerror( ret ) );
+
+	const struct sockaddr_in *addr = reinterpret_cast<struct sockaddr_in *>(res->ai_addr);
+
+	return std::make_pair( addr->sin_addr.s_addr, addr->sin_port );
 }
 
-Core::~Core(void)
+Core::Core(std::vector<Server> const &servers)
 {
-	this->_core.clear();
-	this->_ports.clear();
-	return;
-}
+	std::map< std::pair< uint32_t, uint16_t >, int > serversByHost;
 
-void Core::addServer(Server server)
-{
-	if (this->_core.find(server.getPort()) == this->_core.end())
+	for ( size_t i = 0; i < servers.size(); i++ )
 	{
-		std::vector<Server> servers;
-		servers.push_back(server);
-		this->_core.insert(std::pair<int, std::vector<Server>>(server.getPort(), servers));
+		std::pair < uint32_t, uint16_t > hostAndPort
+			= getHostAndPort( servers[i].getHost(), std::to_string( servers[i].getPort() ) );
+
+		if ( serversByHost.count( hostAndPort ) == 0 )
+		{
+			int fd = socket( AF_INET, SOCK_STREAM, 0 );
+			if ( fd == -1 )
+				throw std::runtime_error( strerror( errno ) );
+
+			this->_serversByFd[fd].push_back( const_cast<Server *>(&servers[i]) );
+			serversByHost[hostAndPort] = fd;
+		}
+		else // serversByHost.count( hostAndPort ) == 1
+		{
+			int fd = serversByHost[hostAndPort];
+			this->_serversByFd[fd].push_back( const_cast<Server *>(&servers[i]) );
+		}
 	}
-	else
-		this->_core[server.getPort()].push_back(server);
-}
 
-void Core::addSocketFd(void)
-{
-	int socketFd;
-
-	for (std::map<int, std::vector<Server>>::iterator it = this->_core.begin(); it != this->_core.end(); it++)
+	typedef std::map< int, std::vector< Server *> >::iterator serversByFdIterator;
+	for ( serversByFdIterator it = this->_serversByFd.begin(); it != this->_serversByFd.end(); it++ )
 	{
-		socketFd = socket(AF_INET, SOCK_STREAM, 0);
-		if (socketFd == -1)
-			throw std::runtime_error("socket() failed");
-		this->_socketFds[socketFd] = it->second;
+		this->_listenFds.insert( it->first );
 	}
-}
 
-std::map<int, std::vector<Server>> const &Core::get_core(void) const
-{
-	return this->_socketFds;
-}
-
-std::map<int, Response> const &Core::get_responses(void) const
-{
-	return this->responses;
-}
-
-bool Core::find_port(int port)
-{
-	return this->_ports.find(port) != this->_ports.end();
-}
-
-void Core::set_pollfds(void)
-{
-	this->_pollfds.clear();
-	for (std::map<int, std::vector<Server>>::iterator it = this->_socketFds.begin(); it != this->_socketFds.end(); it++)
+	typedef std::set< int >::iterator listenFdsIterator;
+	for ( listenFdsIterator it = this->_listenFds.begin(); it != this->_listenFds.end(); it++ )
 	{
-		this->_pollfds.push_back((struct pollfd){
-			.fd = it->first,
-			.events = POLLIN | POLLOUT,
-			.revents = 0});
-		this->_pollFdsSet.insert(it->first);
+		this->addPollFd( *it );
 	}
+
+	this->bindServerSockets();
+
+	return ;
 }
 
-std::vector<struct pollfd> const &Core::get_pollfds(void) const
+void Core::addPollFd( int fd )
 {
-	return this->_pollfds;
-}
-
-std::set<int> const &Core::get_pollFdsSet(void) const
-{
-	return this->_pollFdsSet;
-}
-
-void Core::cleanResponse(int fd)
-{
-	this->responses[fd].clear();
-	this->responses.erase(fd);
-}
-
-void Core::removeRequest(int fd, std::map<int, Request> &Requests)
-{
-	Requests[fd].clear();
-	Requests.erase(fd);
-}
-
-void Core::acceptConnection(const struct pollfd &pfd, int serverFd)
-{
-	int newFd;
-	struct sockaddr_in clientAddr;
-	socklen_t clientAddrLen = sizeof(clientAddr);
-
-	newFd = accept(pfd.fd, (struct sockaddr *)&clientAddr, &clientAddrLen);
-
-	fcntl(newFd, F_SETFL, O_NONBLOCK);
-	std::cout << "Accept new connection with fd = " << newFd << std::endl;
-	if (newFd == -1)
-		throw std::runtime_error("accept() failed" + std::string(strerror(errno)));
-
-	this->_pollfds.push_back((struct pollfd){
-		.fd = newFd,
+	struct pollfd pollFd = (struct pollfd){
+		.fd = fd,
 		.events = POLLIN | POLLOUT,
-		.revents = 0});
+		.revents = 0
+	};
 
-	this->_socketFds[newFd] = this->_socketFds[serverFd];
-
-	this->requests[newFd] = Request();
-	this->responses[newFd] = Response();
+	this->_pollFds.push_back( pollFd );
 }
 
-Server &Core::getServer(int fd, const std::string & host )
+void Core::removePollFd( int fd )
 {
-	const std::vector<Server> &servers = this->_socketFds[fd];
-
-	for (size_t i = 0; i < servers.size(); i++)
+	typedef std::vector< struct pollfd >::iterator pollFdsIterator;
+	for ( pollFdsIterator it = this->_pollFds.begin(); it != this->_pollFds.end(); it++ )
 	{
-		if (servers[i].getServerName() == host)
-			return (Server &)servers[i];
-	}
-
-	return (Server &)servers[0];
-}
-
-std::string myMismatch(std::vector<std::string> v, std::string s)
-{
-	std::map<int, std::string> m;
-
-	for (size_t i = 0; i < v.size(); i++)
-	{
-		if (s == v[i])
-			return v[i];
-	}
-
-	return s;
-}
-
-uint16_t checkUri(Request &req, Server &server)
-{
-	std::string uri = server.directives["root"][0];
-	std::vector<std::string> v;
-
-	for (size_t i = 0; i < server.context.size(); i++)
-	{
-		v.push_back(server.context[i].first);
-	}
-	std::string s = myMismatch(v, req.getUri());
-	std::cerr << "--------------------------------------\n";
-	std::cerr << (uri + s) << std::endl;
-	std::cerr << "--------------------------------------\n";
-	if (access((uri + s).c_str(), F_OK | R_OK) == -1)
-	{
-		perror("access");
-		std::cerr << "Not found" << std::endl;
-		return NOT_FOUND;
-	}
-
-	return OK; // TODO rediretion checking in location
-}
-
-std::vector<std::string> split(std::string &str, char delim, bool keepIt)
-{
-	std::vector<std::string> result;
-	size_t start = 0;
-
-	for (size_t i = 0; i < str.size(); i++)
-	{
-		if (str[i] == delim)
+		if ( it->fd == fd )
 		{
-			if (keepIt)
-				result.push_back(str.substr(start, i - start + 1));
-			else
-				result.push_back(str.substr(start, i - start));
-			start = i + 1;
+			this->_pollFds.erase( it );
+			return ;
 		}
 	}
-	if (start < str.size())
-		result.push_back(str.substr(start));
-
-	return result;
 }
 
-bool Core::readRequest(int fd)
+void Core::start( void )
 {
-	char buffer[MAX_BUFFSIZE + 1];
-	int readBytes;
-
-	bzero(buffer, MAX_BUFFSIZE + 1);
-	readBytes = recv(fd, buffer, MAX_BUFFSIZE, 0);
-	if (readBytes == -1)
-		return (this->requests[fd].status = INTERNAL_SERVER_ERROR, true);
-
-	else if (readBytes == 0)
+	while ( 1337 )
 	{
-		return false;
-	}
-	else
-	{
-		if (this->requests[fd].mainRequest(buffer, readBytes))
+		int ret = poll( this->_pollFds.data(), this->_pollFds.size(), -1 );
+		if ( ret == -1 )
+			throw std::runtime_error( strerror( errno ) );
+
+		int j = 0;
+		for ( size_t i = 0; i < this->_pollFds.size() and j < ret; i++ )
 		{
-			// Server &server = getServer(this->_FdsMap[fd], this->requests[fd].getUri());
-			// this->requests[fd].status = checkUri(this->requests[fd], server);
-			// this->requests[fd].setUri(server.directives["root"][0] + this->requests[fd].getUri());
-			// this->responses[fd].setData(server, this->requests[fd]);
-		}
-	}
-	return false;
-}
+			const int currentFd = this->_pollFds[i].fd;
 
-std::string getDirUri(std::string &uri)
-{
-	std::string s = "<html><head><title>Index of " + uri + "</title></head><body><h1>Index of " + uri + "</h1><hr><pre>";
-	DIR *dir;
-	struct dirent *ent;
-	if ((dir = opendir(uri.c_str())) != NULL)
-	{
-		while ((ent = readdir(dir)) != NULL)
-		{
-			s += "<a href=\"" + std::string(ent->d_name) + "\">" + ent->d_name + "</a><br>";
-		}
-		closedir(dir);
-	}
-	else
-	{
-		perror("");
-		return "";
-	}
-	s += "</pre><hr></body></html>";
-	return "Content-length: " + std::to_string(s.size()) + "\r\n" + s;
-}
-
-// std::string getTypeUri(std::string  uri)
-// {
-// 	struct stat sb;
-
-// 	if (stat(uri.c_str(), &sb) == 0)
-// 	{
-// 		if (S_IFREG & sb.st_mode)
-// 		{
-// 			return ("Content-Type: " + getContentType(uri.substr(uri.find_last_of(".") + 1)) + "\r\nContent-length: " + std::to_string(test(uri).size()) + "\r\n\r\n" + test(uri));
-// 		}
-// 		else
-// 		{
-// 			return ("Content-Type: text/html\r\n" + getDirUri(uri));
-// 		}
-// 	}
-// 	std::cout << uri << " error" << std::endl;
-// 	return "";
-// }
-
-void Core::sendResponse(Request &req, int fd)
-{
-	// TODO: check Host, uri and method
-	Server &server = getServer( fd, req.getHeaders()["host"] );
-
-	if ( req.status == OK )
-	{
-		if (req.getMethod() == "GET")
-		{
-			req.status = checkUri(req, server);
-			req.setUri(server.directives["root"][0] + req.getUri());
-			this->responses[fd].setData(server, req);
-			this->responses[fd].generateResponse();
-			// std::string s = "HTTP/1.1 200 OK\r\n"
-			// 				"Content-Type: text/plain\r\n"
-			// 				"Content-Length: 12\r\n"
-			// 				"\r\n"
-			// 				"Hello world!";
-			// send(fd, s.c_str(), s.length(), 0);
-			// req.state = SENT;
-			// std::cout << "response = " << response.getResponse() << std::endl;
-			ssize_t result = send(fd, responses[fd].getResponse().c_str() + this->responses[fd].bytesSent, responses[fd].getResponse().length() - this->responses[fd].bytesSent, 0);
-			if (result == -1)
+			if ( this->_pollFds[i].revents == 0 )
+				continue ;
+			j++;
+			if ( this->_pollFds[i].revents & POLLIN )
 			{
-				std::cout << "send error" << std::endl;
-				req.status = INTERNAL_SERVER_ERROR;
-				return;
+				if ( this->_listenFds.count( currentFd ) == 1 )
+				{
+					this->acceptNewConnection( currentFd );
+					std::cerr << "Accept new connection" << std::endl;
+				}
+				else
+				{
+					// Read request
+					std::cerr << "Read request" << std::endl;
+				}
 			}
-
-			this->responses[fd].bytesSent += result;
-
-			if ( this->responses[fd].bytesSent == this->responses[fd].getResponse().length() ) req.state = SENT;
-		}
-		else if (req.getMethod() == "POST")
-		{
-		}
-		else if (req.getMethod() == "DELETE")
-		{
 		}
 	}
-	else
-	{
-		Error error(req.status);
-		if ( req.status == BAD_REQUEST )
-			send(fd, error.errorMessage("Bad Request").c_str(), error.errorMessage("Bad Request").length(), 0);
-		else if ( req.status == NOT_FOUND )
-			send(fd, error.errorMessage("Not Found").c_str(), error.errorMessage("Not Found").length(), 0);
-		else if ( req.status == INTERNAL_SERVER_ERROR )
-			send(fd, "HTTP/1.1 500 Internal Server Error\r\n", 36, 0);
-		req.state = SENT;
-	}
 
-	return;
+	return ;
 }
 
-void Core::set_ports(int port)
+void Core::acceptNewConnection( int serverFd )
 {
-	this->_ports.insert(port);
+	//TODO: Save client info for later use (CGI)
+	int newFd = accept( serverFd, NULL, NULL );
+	if ( newFd == -1 )
+		throw std::runtime_error( strerror( errno ) );
+
+	fcntl( serverFd, F_SETFL, O_NONBLOCK );
+	setsockopt( serverFd, SOL_SOCKET, SO_REUSEADDR, NULL, 0 );
+
+	this->addPollFd( newFd );
+
+	this->_serversByFd[newFd] = this->_serversByFd[serverFd];
+
+	// TODO: Create request object
+
+	return ;
+}
+
+void Core::bindServerSockets(void)
+{
+	typedef std::map< int, std::vector< Server *> >::iterator serversByFdIterator;
+	for ( serversByFdIterator it = this->_serversByFd.begin(); it != this->_serversByFd.end(); it++ )
+	{
+		int const fd = it->first;
+		Server * server = it->second.front();
+
+		fcntl( fd, F_SETFL, O_NONBLOCK );
+		setsockopt( fd, SOL_SOCKET, SO_REUSEADDR, NULL, 0 );
+
+		const struct addrinfo hint = (struct addrinfo){
+			.ai_family = AF_INET,
+			.ai_socktype = SOCK_STREAM,
+			.ai_protocol = 0
+		};
+
+		struct addrinfo *res;
+		int ret = getaddrinfo( server->getHost().c_str(), std::to_string( server->getPort() ).c_str(), &hint, &res );
+		if ( ret != 0 )
+			throw std::runtime_error( gai_strerror( ret ) );
+
+		ret = bind( fd, res->ai_addr, res->ai_addrlen );
+		if ( ret == -1 )
+			throw std::runtime_error( strerror( errno ) );
+		freeaddrinfo( res );
+
+		ret = listen( fd, SOMAXCONN );
+		if ( ret == -1 )
+			throw std::runtime_error( strerror( errno ) );
+	}
 }
