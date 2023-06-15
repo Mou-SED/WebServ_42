804b6ed8afc7b05050def24d4ac66582a9044358
Mou-SED <moseddik@student.1337.ma>
2023-06-15 18:25:39 +0100

    feat : GenError pages

--- include/Response.hpp
+++ include/Response.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Response.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 16:20:23 by aaggoujj          #+#    #+#             */
-/*   Updated: 2023/06/14 18:25:30 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2023/06/15 17:48:16 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,24 @@
 #include "UChar.hpp"
 #include "Request.hpp"
 #include "Core.hpp"
+#include "Error.hpp"
 
 class Response
 {
 	private:
-		UChar _body;
-		Request _request;
-		uint16_t _status;
+		UChar 		_body;
+		Request 	_request;
+		uint16_t 	_status;
 		std::string _response;
 		std::string _headers;
 		std::string contentLength;
-		off_t _bodySize;
+		off_t 		_bodySize;
 		std::string _path;
+		// Error 		_error;
 
 	public:
 		char *_buffer;
+		std::vector<std::pair<std::set<int> , std::string> > _error_pages;
 		std::ifstream ifs;
 		size_t bytesSent;
 		Response( void );
@@ -39,19 +42,22 @@ class Response
 
 		std::string	getResponse( void ) const;
 		void		toString( void );
+		void		toString( std::string const &type);
 		void		bodyToString( void );
 		void		generateResponse( void );
-		UChar		openUri( std::string path );
 
 		std::string	getDate( void ) const;
 		std::string getHeaders( void ) const;
 		std::string getPath( void ) const;
 		off_t		getBodySize( void ) const;
 		static		std::string getStatusMessage( uint16_t status );
+		uint16_t	getStatus( void ) const;
 		void 		clear( void );
 
+		void		setErrorPages(std::vector<std::string> &error_pages);
 		void 		setRequest( Request const & req );
 		void 		setBodySize( std::string &path );
+		void		setBodySize( off_t size );
 		void 		setPath( std::string &path );
 
 		void GET( void );
--- src/Response.cpp
+++ src/Response.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Response.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 10:38:37 by aaggoujj          #+#    #+#             */
-/*   Updated: 2023/06/14 18:28:05 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2023/06/15 17:48:10 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,17 +53,20 @@ std::string Response::getDate(void) const
 	return str;
 }
 
-UChar Response::openUri( std::string path )
+void Response::toString( std::string const  &type)
 {
-	std::ifstream ifs(path.c_str());
-
-    return UChar(
-        std::istreambuf_iterator<char>(ifs),
-        std::istreambuf_iterator<char>()
-    );
+	this->_headers += "HTTP/1.1 ";
+	this->_headers += std::to_string(this->_status) + " " + getStatusMessage(this->_status);
+	this->_headers += "\r\n";
+	this->_headers += "Server: WebServ/1.0.0 (Unix)\r\n";
+	this->_headers += "Date: " + getDate() + "\r\n";
+	this->_headers += "Content-Type: " + type + "\r\n";
+	this->_headers += "Content-Length: " + std::to_string(this->_bodySize) + "\r\n";
+	this->_headers += "Connection: close\r\n\r\n";
+	// TODO : Add headers for cookies and redirection.
 }
 
-void Response::toString(void)
+void Response::toString( void )
 {
 	this->_headers += "HTTP/1.1 ";
 	this->_headers += std::to_string(this->_status) + " " + getStatusMessage(this->_status);
@@ -108,6 +111,12 @@ void Response::GET( void )
 
 	std::string path = this->_request.getServer()->getRoot() + uri;
 
+	if ( not this->_request.getServer()->isLocationExist() )
+	{
+		this->_status = NOT_FOUND;
+		goto GENERATE;
+	}
+
 	if ( isDirectory(path) )
 	{
 		std::cerr << "Is Directory" << std::endl;
@@ -117,9 +126,11 @@ void Response::GET( void )
 		// }
 		// else this->_status = NOT_FOUND;
 	}
-	else if ( isFile(path) )
+	else
 	{
-		if ( access(path.c_str(), R_OK) == -1 )
+		if ( access(path.c_str(), F_OK) == -1 )
+			this->_status = NOT_FOUND;
+		else if ( access(path.c_str(), R_OK) == -1 )
 			this->_status = FORBIDDEN;
 		else
 		{
@@ -127,6 +138,16 @@ void Response::GET( void )
 			this->setBodySize(path);
 		}
 	}
+	GENERATE:
+		if ( this->_status >= BAD_REQUEST )
+		{
+			Error error(this->_status, this->_error_pages);
+			this->setBodySize(error.getErrorBody().length());
+			this->_buffer = strdup(error.getErrorBody().c_str());
+			this->toString("text/html");
+		}
+		else
+			this->toString();
 }
 
 std::string Response::getHeaders(void) const
@@ -149,6 +170,30 @@ void Response::clear(void)
 	this->_response.clear();
 }
 
+std::pair<std::set<int>, std::string> getErrorPage(std::string &error_page)
+{
+	std::set<int> status_codes;
+	std::string path;
+
+	std::vector<std::string> tokens = split(error_page, ' ', false);
+	for (size_t i = 0; i < tokens.size(); i++)
+	{
+		if ( tokens[i].find("/") == std::string::npos)
+			status_codes.insert(atoi(tokens[i].c_str()));
+		else
+			path = tokens[i];
+	}
+	return std::make_pair(status_codes, path);
+}
+
+void Response::setErrorPages(std::vector<std::string> &error_pages)
+{
+	for (size_t i = 0; i < error_pages.size(); i++)
+	{
+		this->_error_pages.push_back(getErrorPage(error_pages[i]));
+	}
+}
+
 void Response::setRequest(Request const &req)
 {
 	this->_request = req;
@@ -160,6 +205,11 @@ void Response::setBodySize(std::string &path)
 	struct stat st;
 	stat(path.c_str(), &st);
 	this->_bodySize = st.st_size;
+}
+
+void Response::setBodySize( off_t size )
+{
+	this->_bodySize = size;
 }
 
 void Response::setPath(std::string &path)
@@ -196,4 +246,9 @@ std::string Response::getStatusMessage(uint16_t status)
 		default:
 			throw std::runtime_error("Unknown status code");
 	}
+}
+
+uint16_t Response::getStatus(void) const
+{
+	return this->_status;
 }
