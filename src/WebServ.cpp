db6f35a91da08b392d6597aa4925f6516874e313
Mou-SED <moseddik@student.1337.ma>
2023-06-22 17:33:49 +0100

    feat: Ignore sigPIPE

--- src/WebServ.cpp
+++ src/WebServ.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   webserv.cpp                                        :+:      :+:    :+:   */
+/*   WebServ.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaggoujj <aaggoujj@student.42.fr>          +#+  +:+       +#+        */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 22:02:58 by aaggoujj          #+#    #+#             */
-/*   Updated: 2023/06/13 15:46:11 by aaggoujj         ###   ########.fr       */
+/*   Updated: 2023/06/22 17:27:05 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ bool	webserv(const char *av)
 	std::vector<Server> servers;
 	std::string s(av);
 	std::ifstream file(s);
-
 	if (not file.is_open() or not parcing(file, s))
 		throw std::runtime_error("Error");
 
 	file.open(s.c_str());
 	servers = Tokenization(file);
 
+	signal(SIGPIPE, SIG_IGN);
 	Core core( servers );
 	core.start();
 
