44b75f696a69e64dea9779c1e1ed10064f70da51
junik1337 <ayassir@student.1337.ma>
2023-07-19 09:54:56 +0100

    fix : app.py edited

--- www/html/cgi-bin/app.py
+++ www/html/cgi-bin/app.py
@@ -12,28 +12,51 @@ a = """
 <!DOCTYPE html>
 <html>
 <head>
-<title>Hello, world!</title>
-<style>
-body {
-  font-family: sans-serif;
-  text-align: center;
-}
+  <title>Hello, world!</title>
+  <style>
+    body {
+      font-family: sans-serif;
+      text-align: center;
+      background-color: #f5f5f5;
+      margin: 0;
+      padding: 0;
+    }
 
-h1 {
-  font-size: 2em;
-}
+    .container {
+      max-width: 600px;
+      margin: 40px auto;
+      background-color: #fff;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
 
-p {
-  font-size: 1em;
-}
-</style>
+    h1 {
+      font-family: monospace;
+      font-size: 2.5em;
+      color: #007bff;
+      margin-bottom: 20px;
+    }
+
+    p {
+      font-size: 1.2em;
+      color: #333;
+    }
+
+    .highlight {
+      color: #007bff;
+      font-weight: bold;
+    }
+  </style>
 </head>
 <body>
-<h1>Hello, world!</h1>
-<p>This is a simple HTML for testing CGI</p>
-<p>This is your name = """ + name + """
-<p> This is your age = """ + str(age) + """
-<p> Your email = """ + email + """
+  <div class="container">
+    <h1>Hello, world!</h1>
+    <p>This is a simple HTML for testing CGI</p>
+    <p>This is your name\t: <span class="highlight"> """ + name + """</span></p>
+    <p>This is your age\t: <span class="highlight"> """ + age + """ </span></p>
+    <p>Your email\t: <span class="highlight"> """ + email + """ </span></p>
+  </div>
 </body>
 </html>
 """
